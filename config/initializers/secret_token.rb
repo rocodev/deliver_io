b9f6b849a7abbf6ab61374cd693a57bdcbfc77be
xdite <xuite.joke@gmail.com>
2011-07-29 20:57:41 +0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeliverioFb::Application.config.secret_token = '95352a24dc21819c9a0fa54a59e181070ab31ccbb86af72fec34add6360b29fbfe21ef71444c789946bcbfb9509d260cc81bcc9b14d67568e66bdc62dab7e65d'
